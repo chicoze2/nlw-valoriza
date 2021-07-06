8ac196f3539e11b38736afae78898ea2f59fdcbb
chicoze2 <chicoze247@gmail.com>
2021-07-06 20:28:32 -0300

    CRIADO MIGRATIONS
    -> Estrutura da tabela Users desenvolvida

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,9 @@
+import "reflect-metadata"
+import "./database" //importar o index do DB
 import Express, { response } from 'express';
+
+
 const app = Express(); // no projeto antigo, o express era chamdo de server
 
 
-
-app.get('/', (request, response) => { //req e res
-    return response.send('Olá NLW')
-})
-
-
-app.post('/test', (request, response) => {
-    return response.send('Olá NLW METODO POSTAGEM')
-})
-
-
 app.listen(3000, () => console.log("Server running"))
-
-
