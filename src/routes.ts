901f92657ce0f7e2ef2d7dc81948ebd4e7fa5f9e
Francisco José - chicoze2 <chicoze247@gmail.com>
2021-07-12 18:31:17 -0300

    Aplicação finalizada - Rodando 100%

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import {request, Router} from "express"
+import {Router} from "express"
 
 import {CreateUserController} from "./controllers/CreateUserController";
 import {CreateTagController} from './controllers/CreateTagController';
@@ -6,6 +6,8 @@ import { verificarAdmin } from "./middlewares/verificarAdmin";
 import { verificarAutenticacao } from "./middlewares/verificarAutenticacao";
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateElogioController} from './controllers/CreateElogioController';
+import { ListReceivedElogiosController } from "./controllers/ListReceivedElogioController";
+import { ListSentElogiosController} from './controllers/ListSentElogioController'
 
 const router = Router();
 
@@ -14,11 +16,16 @@ const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createElogioController = new CreateElogioController();
 
+const listSentElogiosController = new ListSentElogiosController()
+const listReceivedElogiosService = new ListReceivedElogiosController()
 
 
 router.post("/users", createUserController.handle);
 router.post('/tags', verificarAutenticacao, verificarAdmin, createTagController.handle);
 router.post('/login', authenticateUserController.handle);
 router.post("/elogios", verificarAutenticacao, createElogioController.handle)
+
+router.get('/users/elogios/enviados', verificarAutenticacao, listSentElogiosController.handle)
+router.get('/users/elogios/recebidos', verificarAutenticacao, listReceivedElogiosService.handle)
 
 export { router }
